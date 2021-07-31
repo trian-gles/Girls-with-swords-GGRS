86bad539041ad229495107ab587a069357abb595
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-07-31 13:33:15 -0400

    basic lobby

---
+++ Lobby/Lobby.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class Lobby : Node2D
+{
+    public void OnHostButtonDown()
+    {
+
+    }
+
+    public void OnJoinButtonDown()
+    {
+
+    }
+
+    public void OnSyncTestButtonDown()
+    {
+
+    }
+}

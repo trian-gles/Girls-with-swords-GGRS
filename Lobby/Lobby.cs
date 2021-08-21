de3da603bbc0a8bee785643e5518c6498c4048dd
kentays <kentay.s@gmail.com>
2021-08-20 20:59:00 -0700

    Hitboxes

--- Lobby/Lobby.cs
+++ Lobby/Lobby.cs
@@ -4,64 +4,64 @@ using System;
 public class Lobby : Node2D
 {
 
-    public override void _Ready()
-    {
-        Globals.Tests();
-    }
-    public void OnHostButtonDown()
-    {
-        
-        Globals.mode = Globals.Mode.GGPO;
-        Begin(true);
-    }
+	public override void _Ready()
+	{
+		Globals.Tests();
+	}
+	public void OnHostButtonDown()
+	{
+		
+		Globals.mode = Globals.Mode.GGPO;
+		Begin(true);
+	}
 
-    public void OnJoinButtonDown()
-    {
-        
-        Globals.mode = Globals.Mode.GGPO;
-        Begin(false);
-    }
+	public void OnJoinButtonDown()
+	{
+		
+		Globals.mode = Globals.Mode.GGPO;
+		Begin(false);
+	}
 
-    public void OnLocalButtonDown()
-    {
-        
-        GD.Print($"Starting local match");
-        Begin(true);
-        Globals.mode = Globals.Mode.LOCAL;
-    }
+	public void OnLocalButtonDown()
+	{
+		
+		GD.Print($"Starting local match");
+		Begin(true);
+		Globals.mode = Globals.Mode.LOCAL;
+	}
 
-    public void OnTrainingButtonDown()
-    {
-        Begin(true);
-        Globals.mode = Globals.Mode.TRAINING;
-    }
+	public void OnTrainingButtonDown()
+	{
+		Begin(true);
+		Globals.mode = Globals.Mode.TRAINING;
+	}
 
-    public void Begin(bool host)
-    {
-        GetNode<Button>("Host").Visible = false;
-        GetNode<Button>("Join").Visible = false;
-        GetNode<Button>("Local").Visible = false;
-        GetNode<Button>("Training").Visible = false;
+	public void Begin(bool host)
+	{
+		GetNode<Button>("Host").Visible = false;
+		GetNode<Button>("Join").Visible = false;
+		GetNode<Button>("Local").Visible = false;
+		GetNode<Button>("Training").Visible = false;
 
-        string ip = "127.0.0.1";
-        int localPort = 0;
-        int otherPort = 0;
+		string ip = "127.0.0.1";
+		int localPort = 0;
+		int otherPort = 0;
 
-        if (Globals.mode == Globals.Mode.GGPO)
-        {
-            ip = GetNode<LineEdit>("OpponentIp").Text;
+		if (Globals.mode == Globals.Mode.GGPO)
+		{
+			ip = GetNode<LineEdit>("OpponentIp").Text;
 
-            otherPort = int.Parse(GetNode<LineEdit>("OpponentPort").Text);
-            localPort = int.Parse(GetNode<LineEdit>("LocalPort").Text);
-            GetNode<LineEdit>("OpponentPort").Visible = false;
-        }
-        
-        GetNode<LineEdit>("OpponentIp").Visible = false;
-        GetNode<LineEdit>("LocalPort").Visible = false;
+			otherPort = int.Parse(GetNode<LineEdit>("OpponentPort").Text);
+			localPort = int.Parse(GetNode<LineEdit>("LocalPort").Text);
+			GetNode<LineEdit>("OpponentPort").Visible = false;
+		}
+		
+		GetNode<LineEdit>("OpponentIp").Visible = false;
+		GetNode<LineEdit>("LocalPort").Visible = false;
 
-        var mainScene = (PackedScene) ResourceLoader.Load("res://MainScene.tscn");
-        var mainInstance = mainScene.Instance() as MainScene;
-        AddChild(mainInstance);
-        mainInstance.Begin(ip, localPort, otherPort, host);
-    }
+		var mainScene = (PackedScene) ResourceLoader.Load("res://MainScene.tscn");
+		var mainInstance = mainScene.Instance() as MainScene;
+		AddChild(mainInstance);
+		mainInstance.Begin(ip, localPort, otherPort, host);
+	}
 }
--- Player/AnimationPlayer.cs
+++ Player/AnimationPlayer.cs
@@ -3,60 +3,60 @@ using System;
 
 public class AnimationPlayer : Godot.AnimationPlayer
 {
-    [Signal]
-    public delegate void AnimationFinished();
+	[Signal]
+	public delegate void AnimationFinished();
 
-    private int animationLength;
-    public int cursor;
-    
-    public void NewAnimation(string animName) 
-    {
-        if (animName == AssignedAnimation) 
-        {
-            
-            Seek(0, true);
-            cursor = 0;
-        }
-        else
-        {
-            Play(animName);
-            cursor = 0;
-            animationLength = (int)CurrentAnimationLength; //Bad idea?
-            Stop();
-            Seek(0, true);
-        }
-    }
+	private int animationLength;
+	public int cursor;
+	
+	public void NewAnimation(string animName) 
+	{
+		if (animName == AssignedAnimation) 
+		{
+			
+			Seek(0, true);
+			cursor = 0;
+		}
+		else
+		{
+			Play(animName);
+			cursor = 0;
+			animationLength = (int)CurrentAnimationLength; //Bad idea?
+			Stop();
+			Seek(0, true);
+		}
+	}
 
-    public void SetAnimationAndFrame(string animName, int frame)
-    {
-        Play(animName);
-        animationLength = (int)CurrentAnimationLength;
-        Stop();
-        cursor = frame;
-        Seek(cursor, true);
-        GD.Print($"Setting animation to {animName} and frame to {frame}");
-    }
-    public void FrameAdvance() 
-    {
-        if (cursor < animationLength)
-        {
-            cursor++;
-            Seek(cursor, true);
-        }
-        else
-        {
-            EmitSignal(nameof(AnimationFinished), CurrentAnimation);
-        }
+	public void SetAnimationAndFrame(string animName, int frame)
+	{
+		Play(animName);
+		animationLength = (int)CurrentAnimationLength;
+		Stop();
+		cursor = frame;
+		Seek(cursor, true);
+		GD.Print($"Setting animation to {animName} and frame to {frame}");
+	}
+	public void FrameAdvance() 
+	{
+		if (cursor < animationLength)
+		{
+			cursor++;
+			Seek(cursor, true);
+		}
+		else
+		{
+			EmitSignal(nameof(AnimationFinished), CurrentAnimation);
+		}
 
-        if (IsPlaying())
-        {
-            GD.Print("This SHOULD NOT BE CALLED");
-        }
-    }
+		if (IsPlaying())
+		{
+			GD.Print("This SHOULD NOT BE CALLED");
+		}
+	}
 
-    public void Restart() 
-    {
-        Seek(0, true);
-        cursor = 0;
-    }
+	public void Restart() 
+	{
+		Seek(0, true);
+		cursor = 0;
+	}
 }

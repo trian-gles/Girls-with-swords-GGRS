b595fc3b01244cc71e672576c45c68ffc826f6eb
kentays <kentay.s@gmail.com>
2021-10-26 23:51:34 -0700

    Run Speed

--- Player/GFXHandler.cs
+++ Player/GFXHandler.cs
@@ -4,25 +4,25 @@ using System.Collections.Generic;
 
 public class GFXHandler : Node
 {
-    private Blood blood;
+	private Blood blood;
 
-    public override void _Ready()
-    {
-        blood = GetNode<Blood>("Blood");
-    }
+	public override void _Ready()
+	{
+		blood = GetNode<Blood>("Blood");
+	}
 
-    public void Effect(string name, Vector2 pos, bool facingRight)
-    {
-        if (name == "Blood")
-        {
-            blood.Trigger(0, pos, facingRight);
-        }
-    }
+	public void Effect(string name, Vector2 pos, bool facingRight)
+	{
+		if (name == "Blood")
+		{
+			blood.Trigger(0, pos, facingRight);
+		}
+	}
 
-    public void Rollback(int frame)
-    {
-        blood.Rollback(frame);
-    }
+	public void Rollback(int frame)
+	{
+		blood.Rollback(frame);
+	}
 
 
 }

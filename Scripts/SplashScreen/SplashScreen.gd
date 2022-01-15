6df40d96379b48aab96bff8ea8f397e2e5e13bdb
kentays <kentay.s@gmail.com>
2022-01-14 16:01:23 -0800

    Splash skippable by joybutton

--- Scripts/SplashScreen/SplashScreen.gd
+++ Scripts/SplashScreen/SplashScreen.gd
@@ -1,23 +1,17 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+# skip splash
 func _input(event):
 	if(event is InputEventKey):
 		go_title_screen()
+		
+	if(event is InputEventJoypadButton):
+		go_title_screen()
+		
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func go_title_screen():
 	get_tree().change_scene("res://Scenes/Lobby.tscn")
 
+#when animation finishes go to title
 func _on_AnimationPlayer_animation_finished(anim_name):
 	go_title_screen()
-	pass # Replace with function body.

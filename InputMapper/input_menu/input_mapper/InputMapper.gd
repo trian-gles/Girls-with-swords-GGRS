fc0b7528253f56537210c408d0326cf8d9e495d2
kentays <kentay.s@gmail.com>
2022-01-15 18:26:09 -0800

    Keyboard Profile Default

--- InputMapper/input_menu/input_mapper/InputMapper.gd
+++ InputMapper/input_menu/input_mapper/InputMapper.gd
@@ -4,12 +4,19 @@ signal profile_changed(new_profile, is_customizable)
 
 var current_profile_id = 0
 var profiles = {
-	0: 'profile_microdash',
+	0: 'profile_keyboard',
 	1: 'profile_fightstick',
-	2: 'profile_custom',
+	2: 'profile_microdash',
+	3: 'profile_custom'
+}
+
+var profile_keyboard = {
+	'p': KEY_Z,
+	'k': KEY_X,
+	's': KEY_C
 }
 var profile_microdash = {
-	'p': JOY_BUTTON_0 & JOY_BUTTON_1,
+	'p': JOY_BUTTON_0,
 	'k': JOY_BUTTON_3,
 	's': JOY_BUTTON_4,
 }
@@ -18,12 +25,12 @@ var profile_fightstick = {
 	'k': JOY_BUTTON_3,
 	's': JOY_BUTTON_5,
 }
-var profile_custom = profile_microdash
+var profile_custom = profile_keyboard
 
 func change_profile(id):
 	current_profile_id = id
 	var profile = get(profiles[id])
-	var is_customizable = true if id == 2 else false
+	var is_customizable = true if id == 3 else false
 	
 	for action_name in profile.keys():
 		change_action_key(action_name, profile[action_name])
@@ -33,9 +40,12 @@ func change_profile(id):
 func change_action_key(action_name, key_scancode):
 	erase_action_events(action_name)
 
-	var new_event = InputEventJoypadButton.new()
-	new_event.set_button_index(key_scancode)
-	InputMap.action_add_event(action_name, new_event)
+	var new_button = InputEventJoypadButton.new()
+	new_button.set_button_index(key_scancode)
+	InputMap.action_add_event(action_name, new_button)
+	var new_key = InputEventKey.new()
+	new_key.set_scancode(key_scancode)
+	InputMap.action_add_event(action_name, new_key)
 	get_selected_profile()[action_name] = key_scancode
 
 func erase_action_events(action_name):

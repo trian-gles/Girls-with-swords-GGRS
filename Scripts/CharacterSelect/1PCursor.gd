3b5892aaee7716cf144e437f542c91773cab8894
kentays <kentay.s@gmail.com>
2022-02-18 05:25:41 -0800

    Character Select

---
+++ Scripts/CharacterSelect/1PCursor.gd
@@ -0,0 +1,36 @@
+extends Sprite
+#object array
+var characters = []
+
+#integer
+var currentSelected = 0
+var currentColumnSpot = 0
+var currentRowSpot = 0
+
+#exports
+export (Texture) var player1Text
+export (Texture) var player2Text
+export (int) var amountOfRows = 1
+export (Vector2) var portraitOffset
+
+#objects
+onready var gridContainer = get_parent().get_node("GridContainer")
+
+func _ready():
+	for nameOfCharacter in get_tree().get_nodes_in_group("Characters"):
+		characters.append(nameOfCharacter)
+	print(characters)
+	texture = player1Text
+	
+func _process(delta):
+	if(Input.is_action_just_pressed("ui_right")):
+		currentSelected += 1
+		currentColumnSpot += 1
+		
+		position.x += portraitOffset.x
+	elif(Input.is_action_just_pressed("ui_left")):
+		currentSelected -= 1
+		currentColumnSpot -= 1
+		
+		position.x -= portraitOffset.x
+		

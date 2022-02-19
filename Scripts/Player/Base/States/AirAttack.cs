39031b32d5b2e2327e3d5c682842de384bd7b01b
trian-gles <69212477+trian-gles@users.noreply.github.com>
2022-02-19 13:32:42 -0500

    hitstun in air glitch

--- Scripts/Player/Base/States/AirAttack.cs
+++ Scripts/Player/Base/States/AirAttack.cs
@@ -25,4 +25,27 @@ public abstract class AirAttack : BaseAttack
 		});
 		AddGatling(new char[] { '8', 'p' }, () => owner.canDoubleJump, "DoubleJump", () => owner.canDoubleJump = false);
 	}
+
+	protected override void EnterHitState(bool knockdown, Vector2 launch, Vector2 collisionPnt)
+	{
+		GetNode<Node>("/root/Globals").EmitSignal(nameof(PlayerFXEmitted), collisionPnt, "hit", false);
+		bool launchBool = false;
+		owner.ComboUp();
+		if (!(launch == Vector2.Zero))
+		{
+			owner.velocity = launch;
+			launchBool = true;
+		}
+
+		bool airState = (launchBool || !owner.grounded);
+
+		if (!knockdown)
+		{
+			EmitSignal(nameof(StateFinished), "CounterFloat");
+		}
+		else
+		{
+			EmitSignal(nameof(StateFinished), "AirKnockdown");
+		}
+	}
 }

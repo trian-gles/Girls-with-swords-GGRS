37ea4881b453230922e875237d40a10f0a9be971
kentays <kentay.s@gmail.com>
2022-02-24 17:30:54 -0800

    Animate CharSelect

--- Scripts/CharacterSelect/CharacterSelectScreen.gd
+++ Scripts/CharacterSelect/CharacterSelectScreen.gd
@@ -1,8 +1,11 @@
 extends Control
 
+onready var animationplayer = $CanvasLayer/AnimationPlayer
+
 func _ready():
 	var cursor = get_node("CanvasLayer/Cursor")
 	cursor.connect("CharacterSelected", self, "_close_screen")
+	
 	
 func _close_screen():
 	queue_free()

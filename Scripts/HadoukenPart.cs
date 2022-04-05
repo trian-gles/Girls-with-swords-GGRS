4518adb10a5880025b63373819fffa26c0ebb75b
trian-gles <69212477+trian-gles@users.noreply.github.com>
2022-04-05 19:13:02 -0400

    fixed infinite hitstun on hadouken

--- Scripts/HadoukenPart.cs
+++ Scripts/HadoukenPart.cs
@@ -15,19 +15,34 @@ public class HadoukenPart : Node2D
 	protected Globals.AttackDetails chDetails;
 
 	[Export]
-	public int hitStun = 10;
+	protected int modifiedHitStun = 0;
 
 	[Export]
-	public int blockStun = 10;
+	protected int modifiedCounterHitStun = 0;
 
 	[Export]
-	public Vector2 launch = new Vector2();
+	protected Vector2 opponentLaunch = Vector2.Zero;
 
 	[Export]
-	public int hitPush = 0;
+	protected Vector2 chLaunch = Vector2.Zero;
 
 	[Export]
-	public int dmg = 1;
+	protected int modifiedHitPush = 0;
+
+	[Export]
+	protected int hitPush = 0;
+
+	[Export]
+	protected State.HEIGHT height = State.HEIGHT.MID;
+
+	[Export]
+	protected BaseAttack.EXTRAEFFECT effect = BaseAttack.EXTRAEFFECT.NONE;
+
+	[Export]
+	protected BaseAttack.EXTRAEFFECT chEffect = BaseAttack.EXTRAEFFECT.NONE;
+
+	[Export]
+	protected bool knockdown = false;
 
 	[Export]
 	public Vector2 speed;
@@ -41,6 +56,36 @@ public class HadoukenPart : Node2D
 	public int creationFrame;
 
 	static private int totalHads;
+
+
+	public override void _Ready()
+	{
+		hitDetails = Globals.attackLevels[level].hit;
+		chDetails = Globals.attackLevels[level].counterHit;
+
+		hitDetails.opponentLaunch = opponentLaunch;
+		if (chLaunch != Vector2.Zero)
+			chDetails.opponentLaunch = chLaunch;
+
+		hitDetails.effect = effect;
+		chDetails.effect = chEffect;
+		hitDetails.knockdown = knockdown;
+		chDetails.knockdown = knockdown;
+		hitDetails.height = height;
+		chDetails.height = height;
+
+		if (modifiedHitStun != 0)
+			hitDetails.hitStun = modifiedHitStun;
+		if (modifiedCounterHitStun != 0)
+			chDetails.hitStun = modifiedCounterHitStun;
+
+		if (modifiedHitPush != 0)
+		{
+			hitDetails.hitPush = modifiedHitPush;
+			chDetails.hitPush = modifiedHitPush;
+
+		}
+	}
 
 	/// <summary>
 	/// Method to be called right after instantiation by the player

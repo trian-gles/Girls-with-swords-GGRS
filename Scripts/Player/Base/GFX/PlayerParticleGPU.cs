efa5b8a51bc8ca3ebfb4d6783d0d53b40b4b54f2
kentays <kentay.s@gmail.com>
2022-04-15 21:24:18 -0700

    GFX

---
+++ Scripts/Player/Base/GFX/PlayerParticleGPU.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class PlayerParticle : CPUParticles2D
+{
+	public int startFrame = 0;
+
+	public void Trigger(int frame, Vector2 pos, bool facingRight)
+	{
+		startFrame = frame;
+		if (facingRight)
+		{
+			Direction = new Vector2(-1, 0);
+		}
+		else 
+		{
+			Direction = new Vector2(1, 0);
+		}
+		Emitting = true;
+		
+
+		Position = pos;
+	}
+
+	public void Rollback(int frame)
+	{
+		if (startFrame > frame)
+		{
+			Emitting = false;
+		}
+	}
+}

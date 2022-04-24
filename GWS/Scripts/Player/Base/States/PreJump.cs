3ce87a972e8c447d421efb2bc802fd02bde49de5
trian-gles <69212477+trian-gles@users.noreply.github.com>
2022-04-24 13:04:25 -0400

    no block on prejump

--- GWS/Scripts/Player/Base/States/PreJump.cs
+++ GWS/Scripts/Player/Base/States/PreJump.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class PreJump : State
 {
 	[Export]
-	public int len = 2;
+	public int len = 3;
 	public override void _Ready()
 	{
 		base._Ready();
@@ -37,6 +37,42 @@ public class PreJump : State
 			
 		else if (inputArr == new char[] { '4', 'p' })
 			owner.velocity.x = -owner.speed;
+	}
+
+	public override void ReceiveHit(Globals.AttackDetails details) // no blocking during prejump
+	{
+		//GD.Print($"Received attack on side {rightAttack}");
+		bool launchBool = false;
+		switch (details.dir)
+		{
+			case BaseAttack.ATTACKDIR.RIGHT:
+				break;
+			case BaseAttack.ATTACKDIR.LEFT:
+				details.opponentLaunch.x *= -1;
+				details.hitPush *= -1;
+				break;
+			case BaseAttack.ATTACKDIR.EQUAL:
+				details.opponentLaunch.x = 0;
+				details.hitPush = 0;
+				break;
+		}
+		owner.hitPushRemaining = details.hitPush;
+		//GD.Print($"Setting hitpush in hitstun to {owner.hitPushRemaining}");
+		owner.velocity = details.opponentLaunch;
+		if (!(details.opponentLaunch == Vector2.Zero))
+		{
+			owner.velocity = details.opponentLaunch;
+			launchBool = true;
+		}
+
+		if (owner.velocity.y < 0) // make sure the player is registered as in the air if launched 
+		{
+			owner.grounded = false;
+		}
+
+
+		EnterHitState(details.knockdown, details.opponentLaunch, details.collisionPnt, details.effect);
+
 	}
 }
 

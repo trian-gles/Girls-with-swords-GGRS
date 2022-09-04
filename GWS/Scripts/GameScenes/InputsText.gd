ce7e25761193dc243121a3bb1aab07f14251706a
trian-gles <69212477+trian-gles@users.noreply.github.com>
2022-09-04 12:27:26 -0400

    improved hitboxes

--- GWS/Scripts/GameScenes/InputsText.gd
+++ GWS/Scripts/GameScenes/InputsText.gd
@@ -1,6 +1,5 @@
 extends Label
 
-
 var last_inputs: int = 0
 
 var displayed_inps = 0
@@ -10,8 +9,7 @@ func clear():
 	var displayed_inps = 0
 	text = ''
 
-func display_input(input):
-	print("Displaying input " + input)
+func display_input(input: String):
 	text = input + '\n' + text
 	displayed_inps += 1
 	
@@ -24,77 +22,78 @@ func display_input(input):
 
 func inputs(inps):
 	## UP
-	if (inps & 1) and not (last_inputs & 1):
+	if (inps & 1) and not(last_inputs & 1):
 		if (inps & 4):
 			display_input('9')
-		elif (inps & 8):
+		elif(inps & 8) :
 			display_input('7')
 		else:
-			display_input('í±»í ¾í±»í ¾í±»8')
+			display_input('8')
 			
-	if !(inps & 1) and (last_inputs & 1):
+	if !(inps & 1) and(last_inputs & 1) :
 		if (inps & 4):
 			display_input('6')
-		elif (inps & 8):
+		elif(inps & 8) :
 			display_input('4')
 			
 	# DOWN
 	
-	if (inps & 2) and not (last_inputs & 2):
+	if (inps & 2) and not(last_inputs & 2):
 		if (inps & 4):
-			display_input('í±»í ¾í±»í ¾í±»3')
-		elif (inps & 8):
-			display_input('í±»í ¾í±»í ¾í±»1')
+			display_input('3')
+		elif(inps & 8) :
+			display_input('1')
 		else:
-			display_input('í±»í ¾í±»í ¾í±»2')
+			display_input("2")
 			
-	if !(inps & 2) and (last_inputs & 2):
+	if !(inps & 2) and(last_inputs & 2) :
 		if (inps & 4):
 			display_input('6')
-		elif (inps & 8):
+		elif(inps & 8) :
 			display_input('4')
 	
 	# RIGHT
 	
-	if (inps & 4) and not (last_inputs & 4):
+	if (inps & 4) and not(last_inputs & 4):
 		if (inps & 1):
 			display_input('9')
-		elif (inps & 2):
+		elif(inps & 2) :
 			display_input('3')
 		else:
 			display_input('6')
 			
-	if !(inps & 4) and (last_inputs & 4):
+	if !(inps & 4) and(last_inputs & 4) :
 		if (inps & 1):
 			display_input('8')
-		elif (inps & 2):
-			display_input('2')
+		elif(inps & 2) :
+			display_input("2")
 			
 	# LEFT
 			
-	if (inps & 8) and not (last_inputs & 8):
+	if (inps & 8) and not(last_inputs & 8):
 		if (inps & 1):
 			display_input('7')
-		elif (inps & 2):
+		elif(inps & 2) :
 			display_input('1')
 		else:
-			display_input('í±»í ¾í±»í ¾í±»4')
+			display_input('4')
 			
-	if !(inps & 8) and (last_inputs & 8):
+	if !(inps & 8) and(last_inputs & 8) :
 		if (inps & 1):
 			display_input('8')
-		elif (inps & 2):
-			display_input('2')
+		elif(inps & 2) :
+			display_input("2")
 			
 	# BUTTONS
 			
-	if (inps & 16) and not (last_inputs & 16):
+	if (inps & 16) and not(last_inputs & 16):
 		display_input('p')
 		
-	if (inps & 32) and not (last_inputs & 32):
+	if (inps & 32) and not(last_inputs & 32):
 		display_input('k')
 		
-	if (inps & 64) and not (last_inputs & 64):
+	if (inps & 64) and not(last_inputs & 64):
 		display_input('s')
-			
+
+
 	last_inputs = inps

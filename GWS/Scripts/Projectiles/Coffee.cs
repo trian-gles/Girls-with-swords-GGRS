4ca1a3011e04262dc6c9a3fbb51fdcabb23c4cf1
trian-gles <69212477+trian-gles@users.noreply.github.com>
2023-12-14 14:05:57 -0500

    Coffee

---
+++ GWS/Scripts/Projectiles/Coffee.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using FixedMath.NET;
+
+
+class Coffee : HadoukenPart
+{
+	Fix64 offset = new Fix64(225);
+	Fix64 mult = new Fix64(32);
+	Fix64 div = new Fix64(8);
+	public override void FrameAdvance()
+	{
+		if (active)
+		{
+			var sloFrame = (Fix64)(frame + 2) / div;
+			var y = Fix64.Sin(sloFrame) * mult + offset;
+			Position = new Vector2(Position.x, (float)Math.Floor((float)y));
+		}
+		
+
+		base.FrameAdvance();
+
+	}
+
+	protected override void HurtPlayer()
+	{
+		base.HurtPlayer();
+		animatedSprite.Play("CoffeeExplosion");
+	}
+}

50acb68d2f17adad54fbb3249c8d2d3a567bc4a6
kentays <kentay.s@gmail.com>
2024-01-04 18:45:14 -0800

    Changes

--- GWS/Scripts/Lobby/LocalMain.gd
+++ GWS/Scripts/Lobby/LocalMain.gd
@@ -2,4 +2,10 @@ extends ToolButton
 
 
 func _ready():
-	grab_focus()
+#	grab_focus()
+	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "fade_in":
+		grab_focus()

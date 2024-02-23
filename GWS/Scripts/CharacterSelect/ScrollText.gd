b42056ac317012517db286a589f5782606f678e8
kentays <kentay.s@gmail.com>
2024-02-22 19:26:16 -0800

    char select screen changes

--- GWS/Scripts/CharacterSelect/ScrollText.gd
+++ GWS/Scripts/CharacterSelect/ScrollText.gd
@@ -1,14 +1,24 @@
 extends Label
-var drawTextSpeed: int = 0
 
-var chatterLimit: int = 56
+var currentCharacter: int = 0
+var delayBetweenCharacters: float = 0.05  # Adjust this value to control the delay between each character
 
-#var chatList: Array = []
+var fullText: String = "SELECT YOUR LADY..."
+var visibleText: String = ""
 
-func _showChatter():
-	if drawTextSpeed < chatterLimit:
-		drawTextSpeed += 1
-		self.visible_characters = drawTextSpeed
-	
-func _process(delta):
-	_showChatter()
+onready var characterTimer := $Timer
+
+func _ready():
+	characterTimer.connect("timeout", self, "_on_Timer_timeout")
+	characterTimer.wait_time = delayBetweenCharacters
+	characterTimer.autostart = true
+
+func _on_Timer_timeout():
+	if currentCharacter < fullText.length():
+		visibleText += fullText[currentCharacter]
+		currentCharacter += 1
+		self.text = visibleText
+		print("Current Text:", visibleText)  # Debugging message
+		print("Current Character Index:", currentCharacter)  # Debugging message
+	else:
+		print("Text fully displayed.")

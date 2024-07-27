7afdb0017e13e86c6a887e6d4929aee50026715b
trian-gles <69212477+trian-gles@users.noreply.github.com>
2024-07-27 14:01:54 +0200

    tool script syncing sprites

---
+++ GWS/Scripts/Tool/SpriteSync.gd
@@ -0,0 +1,18 @@
+tool
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Engine.editor_hint and hframes > 1:
+		frame = owner.get_node("Sprite").frame

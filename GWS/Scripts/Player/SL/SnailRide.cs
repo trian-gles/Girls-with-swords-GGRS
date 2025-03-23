29ff5dfa85494526e08a30a518ca138ff957a6cb
Kieran <bkieranmcauliffe@gmail.com>
2025-03-23 14:29:42 +0100

    Snailride GFX

---
+++ GWS/Scripts/Player/SL/SnailRide.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using System;
+
+public class SnailRide : MovingAttack
+{
+    public override void FrameAdvance()
+    {
+        base.FrameAdvance();
+        if (frameCount % 5 == 0)
+            GetNode<Node>("/root/Globals").EmitSignal(nameof(PlayerFXEmitted),
+			new Vector2(owner.internalPos.x, owner.GetCollisionRect().End.y),
+			"dust", owner.facingRight);
+    }
+}

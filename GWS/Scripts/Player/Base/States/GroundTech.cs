dc0bd6e3d019950c18de7309e02ea60a629280fa
Kieran <bkieranmcauliffe@gmail.com>
2026-08-20 17:13:44 +0200

    Ground tech invuln issue

--- GWS/Scripts/Player/Base/States/GroundTech.cs
+++ GWS/Scripts/Player/Base/States/GroundTech.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class GroundTech : Tech
 {
-    public override string animationName => "Fall";
+    public override string animationName => "Tech";
     public override void HandleInput(InputContainer.CharPair inputArr) // no inputs during ground tech
     {
     }
